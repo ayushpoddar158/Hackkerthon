6d38574356f96a42a799683e4c32abc204464a8d
Ayush Poddar <ayushpoddar158@gmail.com>
2022-08-24 18:13:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fun1=()=>{
+    document.getElementById("form1").style.display="block"
+}
+const fun2=()=>{
+    document.getElementById("form1").style.display="none"
+}
+const fun3=()=>{
+    alert("hello")
+  searchdata=  document.getElementById("search1").value
+  
+  searchdata=searchdata.toLowerCase()
+
+  if(searchdata=="nalanda institute of technology"){
+    document.getElementById("nal").attributes.setNamedItem
+  }
+
+}
